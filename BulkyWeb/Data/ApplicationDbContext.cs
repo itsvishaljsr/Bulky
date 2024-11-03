cf37793fe55213374282fdd9130edd3140798e74
Vishal Prajapati <vishalprajapatijsr@gmail.com>
2024-11-03 22:37:17 +0530

    category table create through code first approach, controller and view also

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Data
 {
@@ -8,6 +9,8 @@ namespace BulkyWeb.Data
         { 
         
         }
+
+        public DbSet<Category> Categories { get; set; } 
         
     }
 }
